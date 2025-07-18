bde73e192ecf52ccce0f72f6d0e77800e4c79989
UP240741 <up240741@alumnos.upa.edu.mx>
2025-07-18 11:42:34 -0600

    Dia_6_Level1.py

--- Dia_6_Level2.py
+++ Dia_6_Level2.py
@@ -1,34 +1,3 @@
-#Level_1
-
-#Ejercicio_1
-tupla_vacia=tuple()
-
-#Ejercicio_2
-hermanas = ("Lucía", "María", "Sofía")
-hermanos = ("Carlos", "Andrés", "Javier")
-
-#Ejercicio_3
-hermanas = ("Lucía", "María", "Sofía")
-hermanos = ("Carlos", "Andrés", "Javier")
-siblings = hermanas + hermanos
-print(siblings)
-
-#Ejercicio_4
-hermanas = ("Lucía", "María", "Sofía")
-hermanos = ("Carlos", "Andrés", "Javier")
-siblings = hermanas + hermanos
-cantidad_hermanos = len(siblings)
-print("Tengo", cantidad_hermanos, "hermanos/hermanas.")
-
-
-#Ejercicio_5
-siblings = ("Lucía", "María", "Sofía", "Carlos", "Andrés", "Javier")
-parents = ("Juan", "Ana")
-family_members = siblings + parents
-print(family_members)
-
-
-
 #Level_2
 
 #Ejercicio_1
@@ -77,8 +46,5 @@ del food_stuff_tp
 nordic_countries = ('Denmark', 'Finland', 'Iceland', 'Norway', 'Sweden')
 print('Estonia' in nordic_countries)  
 print('Iceland' in nordic_countries) 
-
-
-
 
 
