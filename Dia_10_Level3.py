4adb2345f7fc81d123a4d3a1916783e319c76150
UP240741 <up240741@alumnos.upa.edu.mx>
2025-07-18 11:54:35 -0600

    Dia_10_Level1.py

--- Dia_10_Level3.py
+++ Dia_10_Level3.py
@@ -1,71 +1,3 @@
-#Level_1
-
-#Ejercicio_1
-for i in range(11):
-    print(i)
-i = 0
-while i <= 10:
-    print(i)
-    i += 1
-
-#Ejercicio_2
-for i in range(10, -1, -1):
-    print(i)
-i = 10
-while i >= 0:
-    print(i)
-    i -= 1
-
-
-#Ejercicio_3
-for i in range(1, 8):
-    print('#' * i)
-
-#Ejercicio_4
-for _ in range(8):
-    print('# ' * 8)
-
-#Ejercicio_5
-for i in range(11):
-    print(f"{i} x {i} = {i * i}")
-
-
-#Ejercicio_6
-tech = ['Python', 'Numpy', 'Pandas', 'Django', 'Flask']
-for item in tech:
-    print(item)
-
-#Ejercicio_7
-for i in range(0, 101, 2):
-    print(i)
-
-
-#Ejercicio_8
-for i in range(1, 101, 2):
-    print(i)
-
-
-#Level_2
-
-#Ejercicio_1
-total = 0
-for i in range(101):
-    total += i
-print("The sum of all numbers is", total)
-
-
-#Ejercicio_2
-sum_even = 0
-sum_odd = 0
-for i in range(101):
-    if i % 2 == 0:
-        sum_even += i
-    else:
-        sum_odd += i
-print("The sum of all evens is", sum_even)
-print("And the sum of all odds is", sum_odd)
-
-
 #Level_3
 
 #Ejercicio_1
@@ -106,8 +38,5 @@ sorted_countries = sorted(countries_data, key=lambda x: x.get('population', 0), 
 top10 = sorted_countries[:10]
 for country in top10:
     print(country['name'], country.get('population'))
-
-
-
 
 
